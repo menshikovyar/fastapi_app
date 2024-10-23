6c828fbd2f727d914adbc535876d1e9ce7e39f59
Yaroslav Menshikov <menshikovyar@egorych.aero>
2024-10-23 20:17:30 +0300

    Added contents of fastapi-application as a regular directory

---
+++ fastapi-application/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+from api import router as api_router
+from core.config import settings
+
+app = FastAPI ()
+app.include_router(api_router, 
+                   prefix = settings.api.prefix) #пока не понял для чего!!!
+
+@app.get("/")
+def read_root():
+  return {"message" : "Hello!"}
+
+if __name__ == "__main__":
+  uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
